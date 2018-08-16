4c679ce1e7c0df109b08208525c40e0dd375608f
cqing <SiqingIII@163.com>
2018-08-16 10:52:07 +0800

    完成

--- scripts/build/styles.sh
+++ scripts/build/styles.sh
@@ -1,4 +1,12 @@
-for file in client/static/css/*.scss
+# Author : cqing<SiqingIII@163.com>
+# Copyright (c) wusiqing.com
+# Script follows here:
+for rootfile in client/static/css/*.scss
 do
-  node-sass $file  --output-style  compressed  dist/static/styles/$(basename $file .scss).css
+  node-sass $rootfile  --output-style  compressed  dist/$(dirname ${rootfile#*/} )/$(basename  ${rootfile#*/} .scss ).css
+done
+
+for file in client/static/css/**/*.scss
+do
+  node-sass $file  --output-style  compressed  dist/$(dirname ${file#*/} )/$(basename  ${file#*/} .scss ).css
 done
